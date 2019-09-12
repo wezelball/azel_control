daac9460656e47cd4ce480170380b06d0c37ee57
Wezel Ball <wezel@nuc.ball.com>
2019-09-11 22:00:55 -0400

    The RTC is working, but I need it to pass arguments from the procedure, not just print. Also, there is a bug where the azimuth encoder position someetimes overwrites the elevation position.

--- sketchbook/stepper_control_lx200/stepper_control_lx200.ino
+++ sketchbook/stepper_control_lx200/stepper_control_lx200.ino
@@ -33,6 +33,27 @@ long getEncoderPosition(int encoder) {
   return response.toInt();
 }
 
+void getTime()  {
+  // send request to receive data starting at register 0
+  Wire.beginTransmission(0x68); // 0x68 is DS3231 device address
+  Wire.write((byte)0); // start at register 0
+  Wire.endTransmission();
+  Wire.requestFrom(0x68, 3); // request three bytes (seconds, minutes, hours)
+ 
+  while(Wire.available())
+  { 
+    int seconds = Wire.read(); // get seconds
+    int minutes = Wire.read(); // get minutes
+    int hours = Wire.read();   // get hours
+ 
+    seconds = (((seconds & 0b11110000)>>4)*10 + (seconds & 0b00001111)); // convert BCD to decimal
+    minutes = (((minutes & 0b11110000)>>4)*10 + (minutes & 0b00001111)); // convert BCD to decimal
+    hours = (((hours & 0b00100000)>>5)*20 + ((hours & 0b00010000)>>4)*10 + (hours & 0b00001111)); // convert BCD to decimal (assume 24 hour mode)
+ 
+    Serial.print(hours); Serial.print(":"); Serial.print(minutes); Serial.print(":"); Serial.println(seconds);
+  }
+}
+
 void setup() {
  
   // Initialize I2C communications as Master
@@ -40,16 +61,26 @@ void setup() {
   
   // Setup serial monitor
   Serial.begin(9600);
-  Serial.println("I2C Master Demonstration");
+
+  // clear /EOSC bit
+  // Sometimes necessary to ensure that the clock
+  // keeps running on just battery power. Once set,
+  // it shouldn't need to be reset but it's a good
+  // idea to make sure.
+  Wire.beginTransmission(0x68); // address DS3231
+  Wire.write(0x0E); // select register
+  Wire.write(0b00011100); // write register bitmap, bit 7 is /EOSC
+  Wire.endTransmission();
 }
  
 void loop() {
-  delay(1000);
   
   // Print to Serial Monitor
   Serial.print("Azimuth:\t");
   Serial.println(getEncoderPosition(0));
   Serial.print("Elevation:\t");
   Serial.println(getEncoderPosition(1));
-  
+  delay(1000);
+  Serial.print("Time:\t");
+  getTime();
 }
