3a67ca726dd43abfb5b8455c4b956c2b605dbd89
Wezel Ball <wezel@nuc.ball.com>
2019-11-17 19:54:38 -0500

    Updates to work with Python 3 and Wing IDE

--- python/pantilt_master.py
+++ python/pantilt_master.py
@@ -1,10 +1,14 @@
-#have to run 'sudo apt-get install python-smbus'
-#in Terminal to install smbus
+#!/usr/bin/env python3
+
+# pantilt_master.py
+# Dave Cohen
+
+import sys
 import smbus
 import time
 import os
 import datetime
-import ephem
+#import ephem
 import threading
 
 # Set up the i2c bus
@@ -22,57 +26,65 @@ messageQ = []
 
 # This thread is for updating live parameters
 class periodicThread (threading.Thread):
-   def __init__(self, threadID, name):
-      threading.Thread.__init__(self)
-      self.threadID = threadID
-      self.name = name
-      self.running = True
-      self.delay = 0.5
-      self.lastAz, self.lastEl = getEncoders()
-      
-   def run(self):
-      while(self.running):
-         time.sleep(self.delay)
-         variable.azPos, variable.elPos = getEncoders()
-         variable.azVelocity = (variable.azPos - self.lastAz)/self.delay
-         variable.elVelocity = (variable.elPos - self.lastEl)/self.delay
-         self.lastAz,self.lastEl = getEncoders()
-         #Update the logfile
-         log.write(time.strftime('%H:%M:%S') + ',' +  str(variable.azPos) + ',' + str(variable.elPos) + \
-            ',' +  str(variable.azVelocity) + ',' + str(variable.elVelocity) + '\n')
+    def __init__(self, threadID, name):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.running = True
+        self.delay = 0.5
+        self.lastAz, self.lastEl = getEncoders()
 
-   def print_time(self,threadName):
-      print ("%s: %s") % (threadName, time.ctime(time.time()))
-   
-   def stop(self):
-      self.running = False
-      log.close()
+    def run(self):
+        while(self.running):
+            time.sleep(self.delay)
+            variable.azPos, variable.elPos = getEncoders()
+            variable.azVelocity = (variable.azPos - self.lastAz)/self.delay
+            variable.elVelocity = (variable.elPos - self.lastEl)/self.delay
+            self.lastAz,self.lastEl = getEncoders()
+            #Update the logfile
+            log.write(time.strftime('%H:%M:%S') + ',' +  str(variable.azPos) + ',' + str(variable.elPos) + \
+                      ',' +  str(variable.azVelocity) + ',' + str(variable.elVelocity) + '\n')
+
+            # watch for limits reached if homing
+            if variable.azHoming == True:
+                pass
+
+
+
+    def print_time(self,threadName):
+        print ("%s: %s") % (threadName, time.ctime(time.time()))
+
+    def stop(self):
+        self.running = False
+        log.close()
 
 class Variables():
-   def __init__(self):
-      # initialize limit switches
-      self.azCWLimit = False
-      self.azCCWLimit = False
-      self.elUpLimit = False
-      self.elDownLimit = False
-		
-      # Process variables
-      self.azHomingSpeed = 500;
-      self.elHomingSpeed = 500;
-      self.azSlewSpeed = 500;
-      self.elSlewSpeed = 500;
-      self.azAccel = 500;
-      self.elAccel = 500;
-      
-      # Real-time values
-      self.azVelocity = 0
-      self.elVelocity = 0
-      self.azPos = 0
-      self.elPos = 0
-				
-      # homing flags
-      self.homed = False
-      self.homing = False
+    def __init__(self):
+        # initialize limit switches
+        self.azCWLimit = False
+        self.azCCWLimit = False
+        self.elUpLimit = False
+        self.elDownLimit = False
+
+        # Process variables
+        self.azHomingSpeed = 500;
+        self.elHomingSpeed = 500;
+        self.azSlewSpeed = 500;
+        self.elSlewSpeed = 500;
+        self.azAccel = 500;
+        self.elAccel = 500;
+
+        # Real-time values
+        self.azVelocity = 0
+        self.elVelocity = 0
+        self.azPos = 0
+        self.elPos = 0
+
+        # homing flags
+        self.azHomed = False
+        self.azHoming = False
+        self.elHomed = False
+        self.elHoming = False
 
 # ************************* END CLASSES ******************************
 
@@ -91,7 +103,7 @@ def setInitialValues():
 def sendMessage(priority, message, i2c_address):
     messageQ.append((priority, message))
     messageQ.sort(reverse = True)
-    
+
     while messageQ:
         # this is a tuple (priority, message)
         # 1 is the highest priority
@@ -107,11 +119,11 @@ def sendMessage(priority, message, i2c_address):
                 replyBytes = bus.read_i2c_block_data(steppers_i2c_address, 0, 8)
             else:
                 replyBytes = bus.read_i2c_block_data(encoders_i2c_address, 0, 16)
-            
+
             reply = ConvertBytesToString(replyBytes)
         except IOError:
             reply = "Error"
-        
+
         return reply
 
 def ConvertStringToBytes(src):
@@ -124,7 +136,7 @@ def ConvertBytesToString(src):
     result = ""
     for i in src:
         result += chr(i)
-		
+
     return result
 
 # Read the encoder values from the Nano as a tuple
@@ -144,7 +156,7 @@ def getEncoders():
             # This is not a real fix, now the position
             # is wrong - but what do I do?
             encStringList.append(0)
-    
+
     return tuple(encStringList)
 
 # Send a stepper motion command to the Uno
@@ -187,13 +199,13 @@ def setElAccel(accel):
 # Command functions
 def slewNorth():
     print (sendStepperCommand("2:4000"))
-	
+
 def slewEast():
     print(sendStepperCommand("1:4000"))
-	
+
 def slewWest():
     print(sendStepperCommand("1:-4000"))
-	
+
 def slewSouth():
     print(sendStepperCommand("2:-4000"))
 
@@ -213,72 +225,86 @@ def stopEl():
 # and a later move is performed, it starts
 # at full speed with no accel
 def quickStopAz():
-   print(sendStepperCommand("6:0"))
-	
+    print(sendStepperCommand("6:0"))
+
 def quickStopEl():
     print(sendStepperCommand("7:0"))
 
 # returns 0 if limit made
 def isAzCWLimit():
     variable.azCWLimit = sendStepperCommand("8:0")
-    if variable.azCWLimit.find('0') <> -1:
-        print "True"
+    if variable.azCWLimit.find('0') != -1:
+        print ("True")
         return True
-    elif variable.azCWLimit.find('1') <> -1:
-        print "False"
+    elif variable.azCWLimit.find('1') != -1:
+        print ("False")
         return False
-    
+
 
 # returns 0 if limit made	
 def isAzCCWLimit():
     variable.azCCWLimit = sendStepperCommand("9:0")
-    if variable.azCCWLimit.find('0') <> -1:
-        print "True"
+    if variable.azCCWLimit.find('0') != -1:
+        print ("True")
         return True
-    elif variable.azCCWLimit.find('1') <> -1:
-        print "False"
+    elif variable.azCCWLimit.find('1') != -1:
+        print ("False")
         return False
 
 # returns 0 if limit made
 def isElUpLimit():
     variable.elUpLimit = sendStepperCommand("10:0")
-    if variable.elUpLimit.find('0') <> -1:
-        print "True"
+    if variable.elUpLimit.find('0') != -1:
+        print ("True")
         return True
-    elif variable.elUpLimit.find('1') <> -1:
-        print "False"
+    elif variable.elUpLimit.find('1') != -1:
+        print ("False")
         return False
-	
+
 # returns 0 if limit made
 def isElDownLimit():
     variable.elDownLimit = sendStepperCommand("11:0")
-    if variable.elDownLimit.find('0') <> -1:
-        print "True"
+    if variable.elDownLimit.find('0') != -1:
+        print ("True")
         return True
-    elif variable.elDownLimit.find('1') <> -1:
-        print "False"
+    elif variable.elDownLimit.find('1') != -1:
+        print ("False")
         return False
 
-# Process menu command
+# For now, let's home west
+def homeAzimuth():
+    variable.azHoming = True
+    # slew west a long friggin way
+    #sendStepperCommand("1:-20000"))
+    pass
+
+def homeElevation():
+    variable.elHoming = True
+    # slew west a long friggin way
+    #sendStepperCommand("1:-20000"))
+    pass
+
 def processCmd(cmd):
-	switcher = {
-		':Mn#':slewNorth,
-		':Me#':slewEast,
-		':Mw#':slewWest,
-		':Ms#':slewSouth,
-		':Q#':stopAllSlew,
-		'0':printEncoders,
-		'1':stopAz,
-		'2':stopEl,
-		'3':quickStopAz,
-		'4':quickStopEl,
-		'5':isAzCWLimit,
-		'6':isAzCCWLimit,
-		'7':isElUpLimit,
-		'8':isElDownLimit
-	}
-	func=switcher.get(cmd,lambda :'Invalid')
-	return func()
+    switcher = {
+        ':Mn#':slewNorth,
+            ':Me#':slewEast,
+                ':Mw#':slewWest,
+                ':Ms#':slewSouth,
+                ':Q#':stopAllSlew,
+                '0':printEncoders,
+                '1':stopAz,
+                '2':stopEl,
+                '3':quickStopAz,
+                '4':quickStopEl,
+                '5':isAzCWLimit,
+                '6':isAzCCWLimit,
+                '7':isElUpLimit,
+                '8':isElDownLimit,
+                '9':homeAzimuth,
+                '10':homeElevation
+    }
+    func=switcher.get(cmd,lambda :'Invalid')
+    return func()
 
 # loop to send message
 exit = False
@@ -300,13 +326,14 @@ commThread.start()
 setInitialValues()
 
 while not exit:
-    r = raw_input('Enter something, "q" to quit: ')
-    
+
+    r = str(input('-> '))
+
     processCmd(r)
-    
+
     if r=='q':
         exit=True
 
 # Exit program here
 commThread.stop()
-#log.close()
+
